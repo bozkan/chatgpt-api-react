d74e359d7ed3ccc2569e803442b068e53c93bd6c
Bugra Ozkan <bugraozkan@live.com>
2023-11-28 14:27:36 +0000

    Update maxDuration

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -2,6 +2,9 @@
 import axios from 'axios';
 import OpenAI from 'openai';
 
+export const maxDuration = 30; // This function can run for a maximum of 5 seconds
+export const dynamic = 'force-dynamic';
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY, // defaults to process.env["OPENAI_API_KEY"]
 });
